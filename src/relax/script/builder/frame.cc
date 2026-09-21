59ae9bdf404070e144a35ca8922f5bc095ccc463
Tianqi Chen <tqchen@users.noreply.github.com>
2026-09-21 02:47:29 +0000

    Format frame finalization after span member rename

--- src/relax/script/builder/frame.cc
+++ src/relax/script/builder/frame.cc
@@ -82,9 +82,8 @@ void FunctionFrameNode::ExitWithScope() {
         << "A function declaration requires an IRModule frame";
     RelaxFrameNode::ExitWithScope();
     block_builder->EndScope();
-    function =
-        tvm::relax::Function::CreateEmpty(params, ret_ty.value_or(tvm::relax::AnyType()),
-                                          is_pure.value_or(true), DictAttrs(attrs), span);
+    function = tvm::relax::Function::CreateEmpty(params, ret_ty.value_or(tvm::relax::AnyType()),
+                                                 is_pure.value_or(true), DictAttrs(attrs), span);
     if (local) {
       local_var = tvm::Var(name.value(), tvm::relax::GetType(function.value()), span);
     } else {
@@ -98,8 +97,8 @@ void FunctionFrameNode::ExitWithScope() {
       << "A Relax function must have a return value. Please use "
          "`return` to return an Expr";
 
-  Expr body = this->block_builder->Normalize(
-      tvm::relax::SeqExpr(binding_blocks, output.value(), span));
+  Expr body =
+      this->block_builder->Normalize(tvm::relax::SeqExpr(binding_blocks, output.value(), span));
   // if the function is not private, add a global symbol to its attributes
   if (!is_private.value_or(false) && name.has_value() && !attrs.count(tvm::attr::kGlobalSymbol)) {
     attrs.Set(tvm::attr::kGlobalSymbol, name.value());
